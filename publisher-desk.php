600d75daf8bea1fd611b2c573af4cd6542cdb0b4
thepublisherdesk <thepublisherdesk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-21 02:28:28 +0000

    Bug Fix: Closing open <script> tag
    git-svn-id: https://plugins.svn.wordpress.org/the-publisher-desk/trunk@935949 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- publisher-desk.php
+++ publisher-desk.php
@@ -3,7 +3,7 @@
 Plugin Name: The Publisher Desk
 Plugin URI: http://wordpress.org/plugins/the-publisher-desk/
 Description: Allows for easy integration for any Publisher Desk customer using Wordpress.
-Version: 1.0.5
+Version: 1.0.6
 Author: The Publisher Desk
 Author URI: http://www.publisherdesk.com
 License: GPL2
@@ -27,7 +27,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define ( 'PUBLISHER_DESK_VERSION', '1.0.5' );
+define ( 'PUBLISHER_DESK_VERSION', '1.0.6' );
 
 /**
  * Creates the Publisher Desk admin menu link in the sidebar
@@ -55,7 +55,7 @@ function publisher_desk_wp_head() {
     $id = get_option( 'publisher_desk_id' );
 
     $output .= "<script>window.twoOhSixId = '$id';</script>\n";
-    $output .= "<script>window.twoOhSixVersion = '" . PUBLISHER_DESK_VERSION . "';";
+    $output .= "<script>window.twoOhSixVersion = '" . PUBLISHER_DESK_VERSION . "';</script>\n";
     $output .= "<script>\n";
     $output .= "window.twoOhSixCmd = window.twoOhSixCmd || [];\n";
     $output .= "window.twoOhSixCmd.push(function() {\n";
